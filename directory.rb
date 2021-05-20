e89dab34c2e9a3d7c2ec003e0f72cd2ddef1be1c
“Jenny” <jenny0moon@gmail.com>
2021-05-20 09:04:25 +0100

    Add: data storing method

--- directory.rb
+++ directory.rb
@@ -15,6 +15,18 @@ def input_students
   end
 end
 
+def save_students
+  # open the file for writing 
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -25,7 +37,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
 end
 
 def show_students
@@ -40,6 +53,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
